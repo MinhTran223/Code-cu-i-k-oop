23b5dc112dd356de229664f36064b7de3d313621
MinhTran223 <Minh.TA216856@sis.hust.edu.vn>
2024-01-07 21:30:48 +0700

    Revert "..."
    This reverts commit 83c9ebb0795b600614cbaface7ceb8e43fdb6393.

--- My project/Assets/Script/Playerdata.cs
+++ My project/Assets/Script/Playerdata.cs
@@ -1,34 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class PlayerData 
+using UnityEngine.UI;
+[Serializable]
+public class PlayerData
 {
     public string Email;
     public string Username;
     public string Password;
     public string ID;
-    public string Name;
+    public string Name; 
     public int Age;
-    public int score = 0;
-    public int Highscore = 0;
+    public int Score=0;
+    public int Highscore=0;
     public string image;
-    public PlayerData(string email, string userName, string password, string name, int age, string iD, int score, int highscore, string image)
+    public PlayerData(string Username ,string Password, string Email,string ID)
     {
-        this.Email = email;
-        this.Username = userName;
-        this.Password = password;
-        this.ID = iD;
-        this.Name = name;
-        this.Age = age;      
-        this.score = score;
-        this.Highscore = highscore;
-        this.image = image;
-    }
-    public PlayerData(string Email, string userName, string password, string Id) {
         this.Email = Email;
-        this.Username=userName;
-        this.Password=password; 
-        this.ID=Id;
+        this.Username = Username;
+        this.Password = Password;     
+        this.ID = ID;
     }
+    public PlayerData(string Email, string Username, string Password, string ID, string Name,int Age,int Score,int Highscore,string image)
+    {
+        this.Email = Email;
+        this.Username=Username;
+        this.Password=Password;     
+        this.ID = ID;
+        this.Age=Age;     
+        this.Score=Score;
+        this.Highscore=Highscore;
+        this.image=image;               
+    }
+
+
+
+
 }
+
